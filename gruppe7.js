20093766550634d6ad6357f1a707aeab60d7e330
Alberte-Adelhejd <106496912+Alberte-Adelhejd@users.noreply.github.com>
2022-09-07 21:48:35 +0200

    Scroll up

--- gruppe7.js
+++ gruppe7.js
@@ -42,3 +42,21 @@ function myFunction3 (imgs) {
   imgText.innerHTML = imgs.alt;
   expandImg.parentElement.style.display = "block";
 }
+
+/* scroll up */
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
